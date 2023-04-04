5da9b92527c16fcecbed0d54a386fd7bacedea21
João Pedro Dias <jp.souza.dias@gmail.com>
2023-04-04 14:38:17 -0300

    filter

--- secao-13-apendice-a-javascript-moderno/script.js
+++ secao-13-apendice-a-javascript-moderno/script.js
@@ -82,3 +82,30 @@ const user = {
 
 user.sayUserName()
 user.sayUserNameArrow()
+
+// 3 - Filter
+
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+
+const highNumbers = arr.filter((n) => {
+    if (n >= 3) {
+        return n
+    }
+})
+
+console.log(highNumbers)
+
+const users = [
+    { name: 'Matheus', available: true },
+    { name: 'Pedro', available: false},
+    {name: 'João', available: false},
+    {name: 'Marcos', available},
+]
+
+const availableUsers = users.filter((user) => user.available)
+const notAvailableUsers = users.filter((user) => !user.available)
+
+console.log(availableUsers)
+console.log(notAvailableUsers);
