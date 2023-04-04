3ee3c1e64246306296dfed7951129b8c21806a7f
João Pedro Dias <jp.souza.dias@gmail.com>
2023-04-04 16:25:59 -0300

    destructuring

--- secao-13-apendice-a-javascript-moderno/script.js
+++ secao-13-apendice-a-javascript-moderno/script.js
@@ -132,3 +132,23 @@ const age = 21
 
 console.log(`O nome do usuário é ${userName} e ele tem ${age} anos.`)
 console.log('O nome do usuário é ' + userName + ' e ele tem ' + age + ' anos.');
+
+// 6 - destructuring
+const fruits = ["Maçã", "Laranja", "Mamão"]
+
+const [f1, f2, f3] = fruits
+
+console.log(f1);
+
+console.log(f3);
+
+const productDetails = {
+    name: "Mouse",
+    price: 39.99,
+    category: "Periféricos",
+    color: "Cinza"
+}
+
+const {name: productName, price, category: produtcCategory, color} = productDetails
+
+console.log(`o nome do produte é ${productName}, custa R$${price}, pertence a categoria ${produtcCategory}`);
