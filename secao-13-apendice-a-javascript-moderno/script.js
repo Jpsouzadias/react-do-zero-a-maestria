ce2ecc4946ba2d80937546ae32566f89ed8ffcbe
João Pedro Dias <jp.souza.dias@gmail.com>
2023-04-04 17:21:47 -0300

    classe

--- secao-13-apendice-a-javascript-moderno/script.js
+++ secao-13-apendice-a-javascript-moderno/script.js
@@ -172,3 +172,27 @@ const otherInfos = {km: 100000, price: 49000}
 const car = {...carName, ...carBrand, ...otherInfos, wheels: 4}
 
 console.log(car);
+
+// 8 - classe
+class Product {
+    constructor(name, price){
+        this.name = name
+        this.price = price
+    }
+
+    productWithDiscount(discount){
+        return this.price * ((100 - discount) / 100)
+    }
+}
+
+const shirt = new Product("Camisa", 20)
+
+console.log(shirt.name);
+
+console.log(shirt.productWithDiscount(10));
+
+console.log(shirt.productWithDiscount(50));
+
+const tenis = new Product("Tênis", 120)
+
+console.log(tenis.productWithDiscount(20));
