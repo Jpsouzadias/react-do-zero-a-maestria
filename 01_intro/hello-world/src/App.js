052168cf933748db958b60224b9b37e9105c7a68
João Pedro Dias <jp.souza.dias@gmail.com>
2023-04-06 19:08:18 -0300

    desafio-react

--- 01_intro/hello-world/src/App.js
+++ 01_intro/hello-world/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World React!</h1>
-      <h2>Emmet funcionou</h2>
+      <p>Desafio concluido.</p>
+      <h2>Emmet funcionou.</h2>
     </div>
   );
 }
