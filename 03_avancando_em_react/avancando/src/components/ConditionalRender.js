70c9315da10993485e7130169ef6e7ae0aba1d6e
João Pedro Dias <jp.souza.dias@gmail.com>
2023-04-20 14:56:46 -0300

    if-e-else-no-jsx

--- 03_avancando_em_react/avancando/src/components/ConditionalRender.js
+++ 03_avancando_em_react/avancando/src/components/ConditionalRender.js
@@ -3,12 +3,24 @@ import React, { useState } from 'react'
 const ConditionalRender = () => {
     const [x] = useState(false);
 
+    const [name, setName] = useState("Matheus")
+
   return (
     <div>
         <h1>ConditionalRender</h1>
         {x && <p>se x for true, sim!</p>}
         {!x && <p>se x for false, sim!</p>}
-
+        <h1>If ternário</h1>
+        {name === "João" ? (
+          <div>
+            <p>O nome é João</p>
+          </div>
+        ) : (
+          <div>
+            <p>Nome não encontrado</p>
+          </div>
+        )}
+        <button onClick={() => setName("João")}>Clica aqui!</button>
     </div>
   );
 };
