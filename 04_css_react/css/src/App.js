734dafcf6b37dea9590871a05e30271db02576f3
João Pedro Dias <jp.souza.dias@gmail.com>
2023-04-28 18:43:39 -0300

    css-inline

--- 04_css_react/css/src/App.js
+++ 04_css_react/css/src/App.js
@@ -9,6 +9,17 @@ function App() {
       {/* CSS de componente */}
       <MyComponent />
       <p>Este parágrafo é do app.js</p>
+      {/* Inline CSS */}
+      <p style={{ 
+        color: "blue", 
+        padding: "25px", 
+        borderTop: "10px solid red"
+         }}>Este elemento foi estilizado de forma inline</p>
+      <p style={{ 
+        color: "magenta", 
+        padding: "25px", 
+        borderTop: "10px solid red"
+         }}>Este elemento foi estilizado de forma inline</p>
     </div>
   );
 }
