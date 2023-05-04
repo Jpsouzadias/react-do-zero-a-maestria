263574aa3dec648fa21032a14322eac433b50a96
João Pedro Dias <jp.souza.dias@gmail.com>
2023-05-04 19:45:47 -0300

    label-envolvendo-input

--- 05_form_em_react/form/src/Components/MyForm.js
+++ 05_form_em_react/form/src/Components/MyForm.js
@@ -8,8 +8,13 @@ const MyForm = () => {
             <div>
                 <label htmlFor="name">Nome: </label>
                 <input type="text" name="name" placeholder="Digite o seu nome"/>
-                <input type="submit" value="enviar"/>
             </div>
+            {/* 2- label envolvendo input */}
+            <label>
+                <span>E-mail</span> 
+                <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+            </label>
+                <input type="submit" value="enviar"/>
         </form>
     </div>
   )
