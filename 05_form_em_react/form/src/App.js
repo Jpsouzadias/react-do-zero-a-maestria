6528890ec3e5dfb3349c63a3fbd5108dd42f45d5
João Pedro Dias <jp.souza.dias@gmail.com>
2023-05-08 16:06:06 -0300

    select-no-react

--- 05_form_em_react/form/src/App.js
+++ 05_form_em_react/form/src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <h2>Forms</h2>
-      <MyForm user={{ name: "João", email: "joao@gmail.com" }}/>
+      <MyForm user={{ 
+        name: "João", 
+        email: "joao@gmail.com", 
+        bio: "Eu sou o Admin.", 
+        role: "admin" 
+        }}/>
     </div>
   );
 }
--- 05_form_em_react/form/src/Components/MyForm.js
+++ 05_form_em_react/form/src/Components/MyForm.js
@@ -7,8 +7,8 @@ const MyForm = ({ user }) => {
   // 3 - gerenciamento de dados
   const [name, setName] = useState(user ? user.name : "");
   const[email, setEmail]= useState(user ? user.email : "");
-
-  const [bio, setBio] = useState("");
+  const[bio, setBio]= useState(user ? user.bio : "");
+  const[role, setRole]= useState(user ? user.role : "");
 
   const handleName = (e) => {
    setName(e.target.value);
@@ -20,7 +20,7 @@ const MyForm = ({ user }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log("Enviando o formulário");
-    console.log(name, email, bio);
+    console.log(name, email, bio, role);
 
     // validacao
     // envio
@@ -60,7 +60,17 @@ const MyForm = ({ user }) => {
                 <span>Bio:</span>
                 <textarea name="bio" placeholder="Descrição do usuário" onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
               </label>
+                {/* 9 - select */}              
+                <label>
+                  <span>Função do sistema</span>
+                  <select name="role" onChange={(e) => setRole(e.target.value)} value={role} >
+                    <option value="user">Usuário</option>
+                    <option value="editor">Editor</option>
+                    <option value="admin">Administrador</option>
+                  </select>
+                </label>
                 <input type="submit" value="enviar"/>
+
         </form>
     </div>
   )
