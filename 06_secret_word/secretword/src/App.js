1ab5c2d1f5d8adb31fc1f94491cd780612f77c05
João Pedro Dias <jp.souza.dias@gmail.com>
2023-05-12 17:01:44 -0300

    palavra-e-categoria-aleatoria

--- 06_secret_word/secretword/src/App.js
+++ 06_secret_word/secretword/src/App.js
@@ -21,10 +21,47 @@ const stages = [
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
-  const [words] = useState(wordsList)
+  const [words] = useState(wordsList);
+
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    
+    console.log(word);
+
+    return {word, category};
+  }
 
   // starts the secret word game
   const startGame = () => {
+    // pick word and pick category
+    const {word, category } = pickWordAndCategory();
+
+    // creat an array of letters
+    let wordLetters = word.split("");
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase())
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    console.log(word, category);
+
+    // fill states
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
@@ -35,7 +72,7 @@ function App() {
 
   // restarts the game
   const retry = () => {
-    setGameStage(stages[0].name)
+    setGameStage(stages[0].name);
   }
 
   return (
