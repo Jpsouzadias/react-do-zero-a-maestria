c95498ecffcebaeeb1611903dd078500e155cc24
João Pedro Dias <jp.souza.dias@gmail.com>
2023-05-15 18:34:41 -0300

    logica-para-fim-de-jogo

--- 06_secret_word/secretword/src/App.js
+++ 06_secret_word/secretword/src/App.js
@@ -17,7 +17,9 @@ const stages = [
   {id: 1, name: "start"},
   {id: 2, name: "game"},
   {id: 3, name: "end"},
-]
+];
+
+const guessesQty = 3;
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
@@ -29,7 +31,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickWordAndCategory = () => {
@@ -93,14 +95,30 @@ function App() {
         ...actualWrongLetters,
         normalizedLetter
       ]);
+
+      setGuesses((actualGuesses) => actualGuesses - 1);
+    }
+  };
+
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+  }
+
+  useEffect(() => {
+    if (guesses <= 0) {
+      // reset all states
+      clearLetterStates()
+      setGameStage(stages[2].name);
     }
 
-    console.log(guessedLetters);
-    console.log(wrongLetters);
-  }
+  }, [guesses])
 
   // restarts the game
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
     setGameStage(stages[0].name);
   }
 
