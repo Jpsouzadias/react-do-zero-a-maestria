1d515edca9fd0e20764485af5b20c9f8909216d1
João Pedro Dias <jp.souza.dias@gmail.com>
2023-05-26 17:32:58 -0300

    carregando-dados

--- 08_react_router/reactrouter/src/components/Navbar.js
+++ 08_react_router/reactrouter/src/components/Navbar.js
@@ -1,6 +1,7 @@
+// 2 - links com react router
 import "./Navbar.css";
 
-import { Link } from "reacte-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return <nav>
