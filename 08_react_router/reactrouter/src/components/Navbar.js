f3e8ea2595142718395e405297c65f7c9de06534
João Pedro Dias <jp.souza.dias@gmail.com>
2023-06-07 18:14:48 -0300

    link-ativo

--- 08_react_router/reactrouter/src/components/Navbar.js
+++ 08_react_router/reactrouter/src/components/Navbar.js
@@ -1,13 +1,22 @@
 // 2 - links com react router
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <nav>
-        <Link to="/" >Home</Link>
-        <Link to="/about" >Sobre</Link>
-    </nav>;
+  return (
+   <nav>
+       {/*  <Link to="/" >Home</Link>
+        <Link to="/about" >Sobre</Link> */}
+        <NavLink 
+        to="/" 
+        /* className={({ isActive }) => (isActive ? "esta-ativo" : "nao-ativo")} */
+        >
+          Home
+        </NavLink>
+        <NavLink to="/about">Home</NavLink>
+    </nav>
+    );
 }
 
 export default Navbar
