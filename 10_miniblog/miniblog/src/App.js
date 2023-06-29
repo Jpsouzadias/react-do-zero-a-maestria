86b296f514a97f11cfecd514e4bbe0997c8fe052
João Pedro Dias <jp.souza.dias@gmail.com>
2023-06-29 18:28:28 -0300

    configurando-o-react-router

--- 10_miniblog/miniblog/src/App.js
+++ 10_miniblog/miniblog/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home/Home.js';
+import About from './pages/About/About.js';
+
 function App() {
   return (
     <div className="App">
-      <h1>Miniblog</h1>
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/About' element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
