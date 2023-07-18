ff1fe739b0ff68ef0d27855586e541b525a477f7
João Pedro Dias <jp.souza.dias@gmail.com>
2023-07-18 17:55:57 -0300

    concluindo-estrutura-da-dashboard

--- 10_miniblog/miniblog/src/pages/Dashboard/Dashboard.js
+++ 10_miniblog/miniblog/src/pages/Dashboard/Dashboard.js
@@ -13,6 +13,14 @@ const Dashboard = () => {
 
   const {documents: posts, loading} = useFetchDocuments("posts", null, uid);
 
+  const deleteDocument = (id) => {
+
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div>
         <h2>DashBoard</h2>
@@ -25,12 +33,30 @@ const Dashboard = () => {
             </Link>
           </div>
         ) : (
-          <div>
-            <p>Tem posts!</p>
-          </div>
-        )}
+          <>
+            <div>
+              <span>Título</span>
+              <span>Ações</span>
+            </div>
 
-        {posts && posts.map((post) => <h3>{post.title}</h3>)}
+          {posts && 
+            posts.map((post) => (<div key={post.id}>
+              <p>{post.title}</p>
+              <div>
+                <Link to={`/posts/${post.id}`} className='btn btn-outline' >
+                  Ver
+                </Link>
+                <Link to={`/posts/edit/${post.id}`} className='btn btn-outline'>
+                  Editar
+                </Link>
+              <button onClick={() => deleteDocument(post.id)} className='btn btn-outline btn-danger' >
+                Excluir
+              </button>
+              </div>
+            </div>
+            ))}
+          </>
+        )};
     </div>
   );
 };
