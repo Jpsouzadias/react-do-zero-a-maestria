5cd5616f5db86a3e7fa5d4a028bb5b6449be2c7d
João Pedro Dias <jp.souza.dias@gmail.com>
2023-07-20 17:23:15 -0300

    usestate-e-inputs

--- 11_hooks/reacthooks/src/components/HookUseState.js
+++ 11_hooks/reacthooks/src/components/HookUseState.js
@@ -17,7 +17,16 @@ const HookUseState = () => {
     console.log(userName);
 
   }
+  
+  // 2 - useState e input
+  const [ age, setAge ] = useState(18);
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    // envio a uma API
+    console.log(age);
+  }
   return (
     <div>
       {/* 1 - useState */}
@@ -25,6 +34,17 @@ const HookUseState = () => {
         <p>Váriaves: {userName}</p>
         <p>useState: {name}</p>
         <button onClick={changeNames}>Mudar nomes!</button>
+      {/* 2 - useState e input */}
+      <p>Digite a sua idade:</p>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          value={age} 
+          onChange={(e) => setAge(e.target.value)}
+         />
+         <input type="submit" value="Enviar" />
+      </form>
+      <p>Você tem {age} anos!</p>
       <hr />
     </div>
   )
