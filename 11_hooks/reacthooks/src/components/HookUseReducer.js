a872e8f7425b1b76671625034063d5ca150d973f
João Pedro Dias <jp.souza.dias@gmail.com>
2023-07-21 17:50:26 -0300

    Update HookUseReducer.js

--- 11_hooks/reacthooks/src/components/HookUseReducer.js
+++ 11_hooks/reacthooks/src/components/HookUseReducer.js
@@ -1,15 +1,64 @@
-import {useReducer} from 'react';
+import { useReducer, useState } from 'react';
 
 const HookUseReducer = () => {
     // 1 - começando com o useReducer
     const [number, dispatch] = useReducer((state, action) => {
         return Math.random(state);
-    })
+    });
+
+    // 2 - avançando no useReducer
+    const initialTasks = [
+        {id: 1, text: "Fazer algum coisa"},
+        {id: 2, text: "Fazer outra coisa"}
+    ];
+
+    const taskReducer = (state, action) => {
+        switch (action.type) {
+            case 'ADD': 
+                const newTask = {
+                    id: Math.random(),
+                    text: taskText,
+                };
+                
+                setTaskText("")
+                return [...state, newTask]
+                case "DELETE":
+                    return state.filter((task) => task.id !== action.id);
+                default:
+                    return state;
+        }
+    }
+
+    const [taskText, setTaskText] = useState("");
+    const [task, dispatchTask] = useReducer(taskReducer,initialTasks);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        dispatchTask({type: "ADD"});
+    }
+
+    const removeTask = (id) => {
+        dispatchTask({ type: "DELETE", id });
+    };
+
   return (
     <div>
         <h2>useReducer</h2>
         <p>Número: {number}</p>
         <button onClick={dispatch}>Alterar número!</button>
+        <h3>Tarefas:</h3>
+        <form onSubmit={handleSubmit}>
+            <input 
+            type="text" 
+            onChange={(e) => setTaskText(e.target.value)} 
+            value={taskText} 
+            />
+            <input type="submit" value="enviar" />
+        </form>
+        {task.map((task) => (
+            <li key={task.id} onDoubleClick={() => removeTask(task.id)}>{task.text}</li>
+        ))}
         <hr />
     </div>
   )
