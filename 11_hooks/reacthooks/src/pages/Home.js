afc4fb288b6aa49a1bb69b0737b758cc2ca84f82
João Pedro Dias <jp.souza.dias@gmail.com>
2023-07-25 17:35:30 -0300

    usecallback

--- 11_hooks/reacthooks/src/pages/Home.js
+++ 11_hooks/reacthooks/src/pages/Home.js
@@ -7,6 +7,7 @@ import HookUseState from '../components/HookUseState';
 import HookUseReducer from '../components/HookUseReducer';
 import HookUseEffect from '../components/HookUseEffect';
 import HookUseRef from '../components/HookUseRef';
+import HookUseCallback from '../components/HookUseCallback';
 
 
 const Home = () => {
@@ -21,6 +22,7 @@ const Home = () => {
       <p>Valor do context: {contextValue}</p>
       <hr />
       <HookUseRef />
+      <HookUseCallback />
     </div>
   )
 }
