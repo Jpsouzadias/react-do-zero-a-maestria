c92c24a2b3494940bbe5224318ef22b4f8cda619
João Pedro Dias <jp.souza.dias@gmail.com>
2023-07-31 18:05:08 -0300

    correcao

--- 12_reactgram/backend/routes/Router.js
+++ 12_reactgram/backend/routes/Router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express();
 
-router.use("api/users", require("./UserRoutes"));
+router.use("/api/users", require("./UserRoutes"));
 
 // teste route
 router.get("/", (req, res) => {
