74ace58216c9f674b5f89d8be2abb9c4082860f9
João Pedro Dias <jp.souza.dias@gmail.com>
2023-08-03 16:54:26 -0300

    atualizando-usuario

--- 12_reactgram/backend/controllers/UserController.js
+++ 12_reactgram/backend/controllers/UserController.js
@@ -2,6 +2,7 @@ const User = require("../models/User");
 
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 
 const jwtSecret = process.env.JWT_SECRET;
 
@@ -15,19 +16,19 @@ const generateToken = (id) => {
 // Register user and sign in
 const register = async(req, res) => {
     
-    const {name, email, password} = req.body
+    const {name, email, password} = req.body;
     
     // Check if user exists
-    const user = await User.findOne({email})
+    const user = await User.findOne({email});
 
     if(user) {
-        res.status(422).json({errors: ["Por favor, utilize outro e-mail"]})
+        res.status(422).json({errors: ["Por favor, utilize outro e-mail"]});
         return
     }
 
     // Generate password hash
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
     // Create user
     const newUser = await User.create({
@@ -38,7 +39,7 @@ const register = async(req, res) => {
 
     // If user was created successfully, return the token
     if(!newUser) {
-        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]});
         return
     }
 
@@ -52,19 +53,19 @@ const register = async(req, res) => {
 // Sign user ir
 const login = async (req, res) => {
     
-    const {email, password} = req.body
+    const {email, password} = req.body;
 
-    const user = await User.findOne({email})
+    const user = await User.findOne({email});
 
     // Check if user exists
     if(!user) {
-        res.status(404).json({errors: ["Usuário não encontrado."]})
+        res.status(404).json({errors: ["Usuário não encontrado."]});
         return
     }
 
     // Check if password matches
     if(!(await bcrypt.compare(password, user.password))) {
-        res.status(422).json({errors: ["Senha inválida."]})
+        res.status(422).json({errors: ["Senha inválida."]});
         return
     }
 
@@ -79,15 +80,51 @@ const login = async (req, res) => {
 
 // Get current logged in user
 const getCurrentUser = async(req, res) => {
-    const user = req.user
+    const user = req.user;
 
     res.status(200).json(user);
 }
 
 // Update an user
 const update = async (req, res) => {
-    res.send("Update");
-}
+
+    const {name, password, bio} = req.body;
+    
+    let profileImage = null;
+
+    if(req.file) {
+        profileImage = req.file.filename;
+    }
+
+    const reqUser = req.user;
+
+    const user = await User.findById(reqUser._id).select("-password");
+
+    if(name) {
+        user.name = name;
+    }
+
+    if(password) {
+        // Generate password hash
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        user.password = passwordHash;
+    }
+
+    if(profileImage) {
+        user.profileImage = profileImage;
+    }
+
+    if(bio) {
+        user.bio = bio;
+    }
+
+    await user.save()
+
+    res.status(200).json(user);
+
+};
 
 module.exports = {
     register,
