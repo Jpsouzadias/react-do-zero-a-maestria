55f1df6a73e0b25601025cf378b3827d8fbbb6c7
João Pedro Dias <jp.souza.dias@gmail.com>
2023-08-07 17:05:16 -0300

    resgatando-foto-por-id

--- 12_reactgram/backend/controllers/PhotoController.js
+++ 12_reactgram/backend/controllers/PhotoController.js
@@ -86,11 +86,29 @@ const getUserPhotos = async(req, res) => {
         .exec();
 
     return res.status(200).json(photos);
-}
+};
+
+// Get photo by id
+const getPhotoById = async (req, res) => {
+    
+    const {id} = req.params
+    
+    const photo = await Photo.findById(new mongoose.Types.ObjectId(id))
+
+    // Check if photo exists
+    if(!photo) {
+        res.status(404).json({ errors: ["Foto não encontrada!"]});
+        return
+    }
+
+    res.status(200).json(photo);
+
+};
 
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
+    getPhotoById,
 };
