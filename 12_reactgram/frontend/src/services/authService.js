b2cdc0dcb40f439dad929dfb18fa945bf3410c1b
João Pedro Dias <jp.souza.dias@gmail.com>
2023-08-31 11:38:13 -0300

    service-de-login

--- 12_reactgram/frontend/src/services/authService.js
+++ 12_reactgram/frontend/src/services/authService.js
@@ -1,15 +1,15 @@
-import {api, requestConfig} from "../utils/config";
+import { api, requestConfig } from "../utils/config";
 
 // Register an user
-const register = async(data) => {
+const register = async (data) => {
     const config = requestConfig("POST", data)
 
     try {
         const res = await fetch(api + "/users/register", config)
-        .then((res) => res.json())
-        .catch((err) => err);
+            .then((res) => res.json())
+            .catch((err) => err);
 
-        if(res) {
+        if (res) {
             localStorage.setItem("user", JSON.stringify(res));
         }
 
@@ -25,10 +25,32 @@ const logout = () => {
     localStorage.removeItem("user")
 }
 
+// Sign in an user
+const login = async (data) => {
+
+    const config = requestConfig("POST", data)
+
+    try {
+
+        const res = await fetch(api + "/users/login", config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+        if (res) {
+            localStorage.setItem("user", JSON.stringify(res))
+        }
+
+        return res;
+    } catch (error) {
+        console.log(error);
+    }
+
+}
 
 const authService = {
     register,
     logout,
+    login,
 };
 
 export default authService;
