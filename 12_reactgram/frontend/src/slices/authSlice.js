95e2b76202d8cb19ee14027c963f562e4276cfd9
João Pedro Dias <jp.souza.dias@gmail.com>
2023-08-31 11:45:26 -0300

    slice-do-login

--- 12_reactgram/frontend/src/slices/authSlice.js
+++ 12_reactgram/frontend/src/slices/authSlice.js
@@ -28,7 +28,22 @@ export const register = createAsyncThunk("auth/register",
 // Logout an user
 export const logout = createAsyncThunk("auth/logout", async () => {
     await authService.logout();
-})
+});
+
+// Sign in an user
+export const login = createAsyncThunk(
+    "auth/login",
+    async (user, thunkAPI) => {
+        const data = await authService.login(user);
+
+        // check for errors
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0]);
+        }
+
+        return data;
+    }
+);
 
 export const authSlice = createSlice({
     name: 'auth',
@@ -61,6 +76,21 @@ export const authSlice = createSlice({
                 state.loading = false;
                 state.success = true;
                 state.error = null;
+                state.user = null;
+            })
+            .addCase(login.pending, (state) => {
+                state.loading = true;
+                state.error = false;
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.user = action.payload;
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
                 state.user = null;
             })
     },
