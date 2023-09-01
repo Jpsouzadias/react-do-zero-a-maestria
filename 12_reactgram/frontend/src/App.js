8de75c7bac6eb23cd6fcc30261506aec381716dd
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-01 11:38:35 -0300

    estruturando-pagina-de-edicao-de-perfil

--- 12_reactgram/frontend/src/App.js
+++ 12_reactgram/frontend/src/App.js
@@ -34,6 +34,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={auth ? <Home /> : <Navigate to="/login" />} />
+              <Route path="/profile" element={auth ? <EditProfile /> : <Navigate to="/login" />} />
               <Route path="/login" element={!auth ? <Login /> : <Navigate to="/" />} />
               <Route path="/register" element={!auth ? <Register /> : <Navigate to="/" />} />
             </Routes>
