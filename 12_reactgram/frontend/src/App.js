87bce6634d678a1989c921e3081f6edaf2f81a37
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-04 15:54:31 -0300

    criando-pagina-de-postagem-de-fotos

--- 12_reactgram/frontend/src/App.js
+++ 12_reactgram/frontend/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { store } from './store';
 import { Provider } from 'react-redux';
 
-
 // Router
 
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
@@ -19,6 +18,7 @@ import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import EditProfile from './pages/EditProfile/EditProfile';
+import Profile from './pages/Profile/Profile';
 
 function App() {
   const { auth, loading } = useAuth();
@@ -34,10 +34,21 @@ function App() {
           <Navbar />
           <div className="container">
             <Routes>
-              <Route path="/" element={auth ? <Home /> : <Navigate to="/login" />} />
-              <Route path="/profile" element={auth ? <EditProfile /> : <Navigate to="/login" />} />
-              <Route path="/login" element={!auth ? <Login /> : <Navigate to="/" />} />
-              <Route path="/register" element={!auth ? <Register /> : <Navigate to="/" />} />
+              <Route
+                path="/"
+                element={auth ? <Home /> : <Navigate to="/login" />} />
+              <Route
+                path="/profile"
+                element={auth ? <EditProfile /> : <Navigate to="/login" />} />
+              <Route
+                path="/users/:id"
+                element={auth ? <Profile /> : <Navigate to="/login" />} />
+              <Route
+                path="/login"
+                element={!auth ? <Login /> : <Navigate to="/" />} />
+              <Route
+                path="/register"
+                element={!auth ? <Register /> : <Navigate to="/" />} />
             </Routes>
           </div>
           <Footer />
@@ -45,6 +56,6 @@ function App() {
       </Provider>
     </div>
   );
-}
+};
 
 export default App;
