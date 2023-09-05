588c3733b43c45a4a9e756209f2399dbf5011297
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-05 18:07:07 -0300

    formulario-de-postar-foto

--- 12_reactgram/frontend/src/pages/Profile/Profile.js
+++ 12_reactgram/frontend/src/pages/Profile/Profile.js
@@ -23,7 +23,10 @@ const Profile = () => {
     const { user, loading } = useSelector((state) => state.user);
     const { user: userAuth } = useSelector((state) => state.auth);
 
-    // Photo
+    // New form and edit form refs
+    const newPhotoForm = useRef();
+    const editPhotoForm = useRef();
+
 
     // Load user data
     useEffect(() => {
@@ -32,9 +35,11 @@ const Profile = () => {
 
     if (loading) {
         return <p>Carregando...</p>;
-    }
+    };
 
-    // ...
+    const submitHandle = (e) => {
+      e.preventDefault();
+    };
 
 return (
     <div id="profile">
@@ -45,10 +50,29 @@ return (
           <p>Imagem de perfil não disponível</p>
         )}
         <div className="profile-description">
-          <h2>{user ? user.name : 'Nome não disponível'}</h2>
-          <h2>{user ? user.bio : 'Bio não disponível'}</h2>
+          <h2>{user.name}</h2>
+          <h2>{user.bio}</h2>
         </div>
       </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={submitHandle}>
+              <label>
+                <span>Título para a foto:</span>
+                <input type="text" placeholder="Insira um título" />
+              </label>
+              <label>
+                <span>
+                  <input type="file" />
+                </span>
+              </label>
+              <input type="submit" value="Postar" />
+            </form>
+          </div>
+        </>
+      )}
     </div>
   );
   
