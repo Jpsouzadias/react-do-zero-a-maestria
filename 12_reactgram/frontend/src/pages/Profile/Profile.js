e427bda41837e53626348ad7b74e5e2ad2171bab
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-06 13:03:37 -0300

    inserindo-fotos-pelo-formulario

--- 12_reactgram/frontend/src/pages/Profile/Profile.js
+++ 12_reactgram/frontend/src/pages/Profile/Profile.js
@@ -14,34 +14,70 @@ import { useParams } from "react-router-dom";
 
 // Redux
 import { getUserDetails } from "../../slices/userSlice";
+import { publishPhoto, resetMessage } from "../../slices/photoSlice";
 
 const Profile = () => {
-    const { id } = useParams();
+  const { id } = useParams();
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const { user, loading } = useSelector((state) => state.user);
-    const { user: userAuth } = useSelector((state) => state.auth);
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
+  const {
+    photos,
+    loading: loadingPhoto,
+    message: messagePhoto,
+    error: errorPhoto
+  } = useSelector((state) => state.photo);
 
-    // New form and edit form refs
-    const newPhotoForm = useRef();
-    const editPhotoForm = useRef();
+  const [title, setTitle] = useState("")
+  const [image, setImage] = useState("")
 
+  // New form and edit form refs
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
-    // Load user data
-    useEffect(() => {
-        dispatch(getUserDetails(id));
-    }, [dispatch, id]);
+  // Load user data
+  useEffect(() => {
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
 
-    if (loading) {
-        return <p>Carregando...</p>;
-    };
+  if (loading) {
+    return <p>Carregando...</p>;
+  };
 
-    const submitHandle = (e) => {
-      e.preventDefault();
-    };
+  const handleFile = (e) => {
+    const image = e.target.files[0];
 
-return (
+    setImage(image);
+  };
+
+  const submitHandle = (e) => {
+    e.preventDefault();
+
+    const photoData = {
+      title,
+      image
+    }
+
+    // build form data
+    const formData = new FormData()
+
+    const photoFormData = Object.keys(photoData).forEach((key) => formData.append(key, photoData[key])
+    );
+
+    formData.append("photo", photoFormData);
+
+    dispatch(publishPhoto(formData));
+
+    setTitle("");
+
+    setTimeout(() => {
+      dispatch(resetMessage());
+    }, 2000);
+  };
+
+  return (
     <div id="profile">
       <div className="profile-header">
         {user && user.profileImage ? (
@@ -61,21 +97,28 @@ return (
             <form onSubmit={submitHandle}>
               <label>
                 <span>Título para a foto:</span>
-                <input type="text" placeholder="Insira um título" />
+                <input
+                  type="text"
+                  placeholder="Insira um título"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title || ""} />
               </label>
               <label>
                 <span>Imagem:</span>
-                <input type="file" />
-                
+                <input type="file" onChange={handleFile} />
+
               </label>
-              <input type="submit" value="Postar" />
+              {!loadingPhoto && <input type="submit" value="Postar" />}
+              {loadingPhoto && <input type="submit" disabled value="Aguarde..." />}
             </form>
           </div>
+          {errorPhoto && <Message msg={errorPhoto} type="error" />}
+          {messagePhoto && <Message msg={messagePhoto} type="success" />}
         </>
       )}
     </div>
   );
-  
+
 }
 
 export default Profile;
