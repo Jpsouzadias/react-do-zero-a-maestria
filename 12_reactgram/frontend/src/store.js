cc0687bd7662f72df6e3cf0fb1f4451ac0af36f9
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-06 11:08:56 -0300

    iniciando-reducer-de-fotos

--- 12_reactgram/frontend/src/store.js
+++ 12_reactgram/frontend/src/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
 import userReducer from './slices/userSlice';
+import photoReducer from './slices/photoSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer, // Use 'auth' as the key, not 'authSlice'
     user: userReducer,
+    photo: photoReducer,
   },
 });
