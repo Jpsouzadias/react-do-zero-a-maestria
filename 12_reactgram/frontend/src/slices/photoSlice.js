e911ab16c1abf594c0b03014e2ad4d0aca679e3e
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-06 14:12:11 -0300

    slice-fotos-de-usuario

--- 12_reactgram/frontend/src/slices/photoSlice.js
+++ 12_reactgram/frontend/src/slices/photoSlice.js
@@ -26,6 +26,19 @@ export const publishPhoto = createAsyncThunk(
 
         return data;
     }
+);
+
+// Get user photos
+export const getUserPhotos = createAsyncThunk(
+    "photo/userphotos",
+    async (id, thunkAPI) => {
+
+        const token = thunkAPI.getState().auth.user.token;
+
+        const data = await photoService.getUserPhotos(id, token);
+
+        return data;
+    }
 )
 
 export const photoSlice = createSlice({
@@ -41,7 +54,7 @@ export const photoSlice = createSlice({
             .addCase(publishPhoto.pending, (state) => {
                 state.loading = true;
                 state.error = null;
-            }) 
+            })
             .addCase(publishPhoto.fulfilled, (state, action) => {
                 state.loading = false;
                 state.success = true;
@@ -54,6 +67,16 @@ export const photoSlice = createSlice({
                 state.loading = false;
                 state.error = action.payload;
                 state.photo = null;
+            })
+            .addCase(getUserPhotos.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(getUserPhotos.fulfilled, (state, action) => {
+                state.loading = false;
+                state.success = true;
+                state.error = null;
+                state.photos = action.payload;
             })
     }
 });
