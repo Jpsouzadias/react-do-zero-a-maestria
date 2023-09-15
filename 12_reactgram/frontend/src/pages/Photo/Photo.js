33d6d974868b57e8382b322c4878fbb5c6d97adf
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-15 14:39:36 -0300

    finalizando-funcionalidade-de-comentario

--- 12_reactgram/frontend/src/pages/Photo/Photo.js
+++ 12_reactgram/frontend/src/pages/Photo/Photo.js
@@ -15,7 +15,7 @@ import { useParams } from "react-router-dom";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 // Redux
-import { getPhoto, like } from "../../slices/photoSlice";
+import { getPhoto, like, comment } from "../../slices/photoSlice";
 
 const Photo = () => {
   const { id } = useParams();
@@ -27,15 +27,33 @@ const Photo = () => {
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector((state) => state.photo);
 
-  // Comentários
+  const [commentText, setCommentText] = useState("")
 
   // Load photo data
   useEffect(() => {
     dispatch(getPhoto(id));
   }, [dispatch, id]);
 
+  // Insert a like
   const handleLike = () => {
     dispatch(like(photo._id));
+
+    resetMessage();
+  };
+
+  // Insert a comment
+
+  const handleComment = (e) => {
+    e.preventDefault();
+
+    const commentData = {
+      comment: commentText,
+      id: photo._id
+    };
+
+    dispatch(comment(commentData));
+
+    setCommentText("");
 
     resetMessage();
   };
@@ -50,6 +68,38 @@ const Photo = () => {
       {error && <Message msg={error} type="error" />}
       {message && <Message msg={message} type="success" />}
     </div>
+    {photo.comments && (
+      <>
+        <div className="comments">
+          <h3>Comentários: ({photo.comments ? photo.comments.length : 0})</h3>
+          <form onSubmit={handleComment}>
+            <input
+              type="text"
+              placeholder="Insira o seu comentário..."
+              onChange={(e) => setCommentText(e.target.value)}
+              value={commentText || ""}
+            />
+            <input type="submit" value="Enviar" />
+          </form>
+          {photo.comments &&
+            photo.comments.length === 0 && <p>Não há comentários...</p>}
+          {photo.comments &&
+            photo.comments.map((comment) => (
+              <div className="comment" key={comment.comment}>
+                <div className="author">
+                  {comment.userImage && (
+                    <img src={`${uploads}/users/${comment.userImage}`} alt={comment.userName} />
+                  )}
+                  <Link to={`/users/${comment.userId}`}>
+                    <p>{comment.userName}</p>
+                  </Link>
+                </div>
+                <p>{comment.comment}</p>
+              </div>
+            ))}
+        </div>
+      </>
+    )}
   </div>;
 };
 
