228d5daec10bdaa55a3281436531f837572cd763
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-19 13:22:43 -0300

    service-de-busca

--- 12_reactgram/frontend/src/services/photoService.js
+++ 12_reactgram/frontend/src/services/photoService.js
@@ -119,6 +119,20 @@ const getPhotos = async (token) => {
     }
 };
 
+// Search photo by title
+const searchPhotos = async (query, token) => {
+    const config = requestConfig("GET", null, token)
+
+    try {
+        const res = await fetch(api + "/photos/search?q=" + query, config)
+            .then((res) => res.json())
+            .catch((err) => err);
+
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 const photoService = {
     publishPhoto,
     getUserPhotos,
@@ -128,6 +142,7 @@ const photoService = {
     like,
     comment,
     getPhotos,
+    searchPhotos,
 };
 
 export default photoService;
