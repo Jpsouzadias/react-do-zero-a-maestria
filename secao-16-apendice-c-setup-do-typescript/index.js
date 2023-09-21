dae86ee42451ecca2aa8c3a6f86cb97f536253c4
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-21 16:57:46 -0300

    adicao-virgula

--- secao-16-apendice-c-setup-do-typescript/index.js
+++ secao-16-apendice-c-setup-do-typescript/index.js
@@ -2,7 +2,7 @@ var firstName = "João";
 var anotherName = 1;
 var x = true;
 function greeting(name) {
-    console.log("Olá " + name);
+    console.log("Olá, " + name);
 }
 greeting(firstName);
 /* greeting(anotherName)
--- secao-16-apendice-c-setup-do-typescript/index.ts
+++ secao-16-apendice-c-setup-do-typescript/index.ts
@@ -3,7 +3,7 @@ const anotherName = 1
 const x = true
 
 function greeting(name: string) {
-    console.log("Olá " + name);
+    console.log("Olá, " + name);
 }
 
 greeting(firstName)
