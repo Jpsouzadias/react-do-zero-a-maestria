17c65910835f3cd82ba442f7ace6bcad48442ed3
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-22 13:19:47 -0300

    variaveis-no-react

--- secao-17-apendice-d-react-com-typescript/react_com_ts/src/App.tsx
+++ secao-17-apendice-d-react-com-typescript/react_com_ts/src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
 function App() {
+
+  // 1 - variaveis
+  const name: string = "João";
+  const age: number = 21
+  const isWorking: boolean = true;
+
   return (
     <div className="App">
       <h1>TypeScript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>)}
     </div>
   );
 }
