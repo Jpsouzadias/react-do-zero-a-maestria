cd1418c7cc30e389d4d12967562d304ab128bba2
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-27 17:32:20 -0300

    exibindo-lista-de-tarefas

--- secao-18-apendice-e-projeto-de-react-com-typescript/todo_react/src/components/TaskList.tsx
+++ secao-18-apendice-e-projeto-de-react-com-typescript/todo_react/src/components/TaskList.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 
-type Props = {}
+// Interface
+import { ITask } from '../interfaces/Task';
 
-const TaskList = (props: Props) => {
-  return (
-    <div>
-        <p>lista de tarefas</p>
-    </div>
-  )
+// CSS
+import styles from './TaskList.module.css';
+
+type Props = {
+  taskList: ITask[];
 }
+
+const TaskList = ({ taskList }: Props) => {
+  return (
+    <>
+      {taskList.length > 0 ? (
+        taskList.map((task) => (
+          <div key={task.id}>
+            <p>{task.title}</p>
+          </div>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas!</p>
+      )}
+    </>
+  );
+};
 
 export default TaskList
