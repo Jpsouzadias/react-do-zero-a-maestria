b2bc1c5b534d02424d7ccf06f51590ab4393e698
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-27 13:08:01 -0300

    css-do-form

--- secao-18-apendice-e-projeto-de-react-com-typescript/todo_react/src/components/TaskForm.tsx
+++ secao-18-apendice-e-projeto-de-react-com-typescript/todo_react/src/components/TaskForm.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+
+// CSS
+import styles from "./TaskForm.module.css";
 
 type Props = {
     btnText: string;
@@ -6,12 +9,12 @@ type Props = {
 
 const TaskForm = ({ btnText }: Props) => {
     return (
-        <form>
-            <div>
+        <form className={styles.form}>
+            <div className={styles.input_container}>
                 <label htmlFor="title">Título:</label>
                 <input type="text" name="title" placeholder='Título da tarefa' />
             </div>
-            <div>
+            <div className={styles.input_container}>
                 <label htmlFor="difficulty">Dificuldade:</label>
                 <input type="text" name="difficulty" placeholder='Dificuldade da tarefa' />
             </div>
