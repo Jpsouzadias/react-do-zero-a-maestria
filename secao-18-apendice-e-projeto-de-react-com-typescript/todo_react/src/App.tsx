d4fe6254fcae87daf49452647943e3735276b472
João Pedro Dias <jp.souza.dias@gmail.com>
2023-09-28 16:33:49 -0300

    evento-para-fechar-modal

--- secao-18-apendice-e-projeto-de-react-com-typescript/todo_react/src/App.tsx
+++ secao-18-apendice-e-projeto-de-react-com-typescript/todo_react/src/App.tsx
@@ -25,9 +25,24 @@ function App() {
     );
   };
 
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal")
+    if (display) {
+      modal!.classList.remove("hide")
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (): void => {
+    hideOrShowModal(true);
+  }
+
   return (
     <div>
-      <Modal children={<TaskForm btnText='Editar tarefa' taskList={taskList} />} />
+      <Modal
+        children={<TaskForm btnText='Editar tarefa' taskList={taskList} />}
+      />
       <Header />
       <main className={styles.main}>
         <div>
@@ -40,7 +55,11 @@ function App() {
         </div>
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList} handleDelete={deleteTask} />
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
         </div>
       </main>
       <Footer />
